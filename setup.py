29574ead752f9007df97a8b9a09e8d9df4f6a17a
cstorm125 <cebril@gmail.com>
2021-06-15 23:21:12 +0700

    remove pickle from install_requires

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from setuptools import setup
 setup(
     name='thai_sentiment',
     packages=['thai_sentiment'],
-    version='v0.1.2',  # Ideally should be same as your GitHub release tag varsion
+    version='v0.1.3',  # Ideally should be same as your GitHub release tag varsion
     description='The naive sentiment classification function based on NBSVM trained on wisesight_sentiment',
     author='cstorm125',
     author_email='cebril@gmail.com',
     url='https://github.com/cstorm125/thai_sentiment',
-    download_url='https://github.com/cstorm125/thai_sentiment/archive/refs/tags/v0.1.2.tar.gz',
+    download_url='https://github.com/cstorm125/thai_sentiment/archive/refs/tags/v0.1.3.tar.gz',
     keywords=['sentiment analysis', 'thai', 'nlp'],
     classifiers=[],
     install_requires=[
         'pythainlp',
         'sklearn',
-        'pickle',
         'scipy',
     ],
     include_package_data=True,
